35c384e9b36146817d2d53b7493667318056859c
soubj <sobujahmed124@gmail.com>
2023-06-19 23:49:03 +0600

    fixed fetch error and put alert

--- src/components/Update.jsx
+++ src/components/Update.jsx
@@ -3,34 +3,37 @@ import { useLoaderData } from "react-router-dom";
 const Update = () => {
   const loadedUser = useLoaderData();
 
-    const handleUpdate = event =>{
-        event.preventDefault();
+  const handleUpdate = (event) => {
+    event.preventDefault();
 
-        const form = event.target;
-        const name = form.name.value;
-        const email = form.email.value;
-        const updatedUser = {name, email};
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const updatedUser = { name, email };
 
-        fetch(`http://localhost:5000/users/${loadedUser._id}`,{
-            method: "PUT",
-            headers:{
-                "content-type": "application/json",
-            },
-            body: JSON.stringify(updatedUser)
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-        })
-    }
+    fetch(`http://localhost:5000/users/${loadedUser._id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          alert("user updated successfully");
+        }
+      });
+  };
 
   return (
     <div>
-      <h3>Update data of {loadedUser.name}</h3>
+      <h3>Update data of {loadedUser?.name}</h3>
       <form onSubmit={handleUpdate}>
-        <input type="text" name="name" defaultValue={loadedUser.name} />
+        <input type="text" name="name" defaultValue={loadedUser?.name} />
         <br />
-        <input type="email" name="email" defaultValue={loadedUser.email} />
+        <input type="email" name="email" defaultValue={loadedUser?.email} />
         <br />
         <input type="submit" value="Update" />
       </form>
