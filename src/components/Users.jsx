5217a2ffe7b8577aa72b5925b064781f528673d3
soubj <sobujahmed124@gmail.com>
2023-06-19 20:53:43 +0600

    delete specific document from database

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,13 +3,29 @@ import { Link, useLoaderData } from "react-router-dom";
 const Users = () => {
   const user = useLoaderData();
 
+  const handleDelete = (_id) => {
+    console.log("delete", _id);
+
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("successfully deleted from database.");
+        }
+      });
+  };
+
   return (
     <div>
       <h1>{user.length}</h1>
       <div>
         {user.map((u) => (
           <p key={u._id}>
-            {u.name} : {u.email}
+            {u.name} : {u.email} : {u._id}
+            <button onClick={() => handleDelete(u._id)}>X</button>
           </p>
         ))}
       </div>
